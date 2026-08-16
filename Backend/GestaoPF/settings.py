9d3ea0ae061b7afe92613659f671cbfa58a85fbc
ManubsProgramming <manuela.j.gamer1968@gmail.com>
2026-08-16 11:34:07 -0400

    Configura PostgreSQL Railway

--- Backend/GestaoPF/settings.py
+++ Backend/GestaoPF/settings.py
@@ -143,18 +143,32 @@ WSGI_APPLICATION = 'GestaoPF.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASE_URL = os.environ.get(
-    "DATABASE_URL"
-)
-
-
-if DATABASE_URL:
+if os.environ.get("PGHOST"):
     DATABASES = {
-        "default": dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            conn_health_checks=True,
-        )
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+
+            "NAME": os.environ.get(
+                "PGDATABASE"
+            ),
+
+            "USER": os.environ.get(
+                "PGUSER"
+            ),
+
+            "PASSWORD": os.environ.get(
+                "PGPASSWORD"
+            ),
+
+            "HOST": os.environ.get(
+                "PGHOST"
+            ),
+
+            "PORT": os.environ.get(
+                "PGPORT",
+                "5432",
+            ),
+        }
     }
 
 else:
