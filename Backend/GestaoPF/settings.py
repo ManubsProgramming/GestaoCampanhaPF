d34eeaddf58b15905bffd97828a6aa8f08b210ea
ManubsProgramming <manuela.j.gamer1968@gmail.com>
2026-08-22 13:47:12 -0400

    privacidade ok

--- Backend/GestaoPF/settings.py
+++ Backend/GestaoPF/settings.py
@@ -26,6 +26,11 @@ load_dotenv(
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+
 
 SECRET_KEY = os.environ.get(
     "SECRET_KEY",
@@ -110,8 +115,7 @@ if not DEBUG:
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-
-
+    
 
 
 CORS_ALLOW_ALL_ORIGINS = False
